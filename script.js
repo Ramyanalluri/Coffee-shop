f1465c44fefb628cd9e1b08cec5bb88c97753d4c
Ramyanalluri <rnalluri@rivier.edu>
2024-08-10 23:06:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        alert('Thank you for your message!');
+    const orderButton = document.getElementById('order-button');
+    orderButton.addEventListener('click', () => {
+        const espressoQuantity = document.getElementById('espresso-quantity').value;
+        const latteQuantity = document.getElementById('latte-quantity').value;
+        const cappuccinoQuantity = document.getElementById('cappuccino-quantity').value;
+        const mochaQuantity = document.getElementById('mocha-quantity').value;
+
+        alert(`Order Summary:
+        Espresso: ${espressoQuantity}
+        Latte: ${latteQuantity}
+        Cappuccino: ${cappuccinoQuantity}
+        Mocha: ${mochaQuantity}`);
     });
 });
